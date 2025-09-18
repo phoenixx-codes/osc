a36b8bec1bca4df1741f6412f0af305fd643fe73
sathwikv2005 <sathwikv2005@gmail.com>
2025-09-18 20:32:15 +0530

    integrate mongoDB

--- backend/app.py
+++ backend/app.py
@@ -5,19 +5,40 @@ from routes.contact import contact_bp
 from routes.events import events_bp
 from routes.team import team_bp
 
-app = Flask(__name__)
-CORS(app)
+import os
+from mongoDB import mongo
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-app.register_blueprint(comingsoon_bp, url_prefix="/api")
-app.register_blueprint(contact_bp, url_prefix="/api")
-app.register_blueprint(events_bp, url_prefix="/api")
-app.register_blueprint(team_bp, url_prefix="/api")
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+ 
+    #get mongoDB url from .env
+    app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+    #initialize PyMongo with the app
+    mongo.init_app(app)
+
+    from routes.comingsoon import comingsoon_bp
+    from routes.contact import contact_bp
+    from routes.events import events_bp
+    from routes.team import team_bp
+
+    app.register_blueprint(comingsoon_bp, url_prefix="/api")
+    app.register_blueprint(contact_bp, url_prefix="/api")
+    app.register_blueprint(events_bp, url_prefix="/api")
+    app.register_blueprint(team_bp, url_prefix="/api")
+
+    @app.route("/")
+    def home():
+        return "Yoo wsg mate"
+
+    return app
 
 
-@app.route("/")
-def home():
-    return "Yoo wsg mate"
-
-if __name__=="__main__":
+if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
