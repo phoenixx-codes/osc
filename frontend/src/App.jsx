4571986b63ac5aced524c67016886f8198477955
sathwikv2005 <sathwikv2005@gmail.com>
2025-09-22 18:59:20 +0530

    feat: handle 404 errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import Home from './pages/Home'
 import Team from './pages/Team'
 import Events from './pages/Events'
 import Contact from './pages/Contact'
+import ComingSoon from './components/ComingSoon'
+import Error404 from './components/Error404'
 
 function App() {
 	return (
@@ -13,10 +15,12 @@ function App() {
 			<Routes>
 				<Route path="/" element={<GradientLayout />}>
 					<Route index element={<Home />} />
-
 					<Route path="team" element={<Team />} />
 					<Route path="events" element={<Events />} />
 					<Route path="contact" element={<Contact />} />
+					<Route path="coming-soon" element={<ComingSoon />} />
+					{/* 404 route */}
+					<Route path="*" element={<Error404 />} />
 				</Route>
 			</Routes>
 		</Router>
